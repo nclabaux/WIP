78de8a35825920548a47bd88ecf485f1d2041299
Nathan CLABAUX <nclabaux@e3r7p3.42.fr>
2020-03-31 16:29:37 +0200

    seem ok

--- gnl_1/main.c
+++ gnl_1/main.c
@@ -9,17 +9,17 @@ int main()
 	int verif;
 
 	line = NULL;
-	fd = open("tester", O_RDONLY);
-//	fd = open("words.txt", O_RDONLY);
-    //	fd = open("5k_char_line", O_RDONLY);
+//	fd = open("tester", O_RDONLY);
+	fd = open("words.txt", O_RDONLY);
+//	fd = open("5k_char_line", O_RDONLY);
 //	fd = 0;
-/*	while((verif = get_next_line(fd, &line)))
+	while((verif = get_next_line(fd, &line)))
 	{
 		printf("%d\t%s\n", verif, line);
 		if (verif == -1)
 			break;
 	}
-*/	verif = get_next_line(fd, &line);
+	verif = get_next_line(fd, &line);
 	printf("%d\t%s\n", verif, line);
 	close(fd);
 	free(line);
