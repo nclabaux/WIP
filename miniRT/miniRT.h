993bf1a1f6a2a7b12e95f8807e7a8668d3bd8923
Nathan CLABAUX <nclabaux@e3r6p15.42.fr>
2020-04-01 17:37:09 +0200

    miniRT init

---
+++ miniRT/miniRT.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   miniRT.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nclabaux </var/mail/nclabaux>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/04/01 17:31:43 by nclabaux          #+#    #+#             */
+/*   Updated: 2020/04/01 17:34:28 by nclabaux         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINIRT_H
+# define MINIRT_H
+
+# include <fcntl.h>
+# include <unistd.h>
+# include <sys/types.h>
+# include <sys/uio.h>
+# include <stdlib.h>
+# include <stdio.h>
+# include <string.h>
+
+
+#endif
