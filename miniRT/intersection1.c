121a9c788592f0dc6f0059ee4dfae4d67804a534
nclabaux <nclabaux@student.42.fr>
2020-07-29 19:56:00 +0200

    cyl

--- miniRT/intersection1.c
+++ miniRT/intersection1.c
@@ -6,7 +6,7 @@
 /*   By: nclabaux <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/05/22 17:09:25 by nclabaux          #+#    #+#             */
-/*   Updated: 2020/07/28 13:25:18 by nclabaux         ###   ########.fr       */
+/*   Updated: 2020/07/29 18:12:46 by nclabaux         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,14 +30,20 @@ t_intersec	ft_cy_side(t_ray ray, t_cylinder cy)
 	double	root[2];
 	t_intersec	res;
 	t_intersec	storage;
+	double	shit[6];
 	t_td	u;
 
 	res.dist = -1;
 	storage.dist = -1;
-	u = ft_2p_to_v(cy.p, ray.p);
-	coef[0] = ft_dot(ray.v, ray.v) - ft_sq(ft_dot(ray.v, cy.v));
-	coef[1] = 2 * (ft_dot(ray.v, u) - ft_dot(ray.v, cy.v) * ft_dot(u, cy.v));
-	coef[2] = -(ft_dot(u, u) + ft_sq(ft_dot(u, cy.v)) + ft_sq(cy.d / 2));
+	shit[0] = ray.v.y * cy.v.z - ray.v.z * cy.v.y;
+	shit[1] = ray.p.y * cy.v.z - ray.p.z * cy.v.y;
+	shit[2] = ray.v.z * cy.v.x - ray.v.x * cy.v.z;
+	shit[3] = ray.p.z * cy.v.x - ray.p.x * cy.v.z;
+	shit[4] = ray.v.x * cy.v.y - ray.v.y * cy.v.x;
+	shit[5] = ray.p.x * cy.v.y - ray.p.y * cy.v.x;
+	coef[0] = ft_sq(shit[0]) + ft_sq(shit[2]) + ft_sq(shit[4]);
+	coef[1] = 2 * (shit[0] * shit[1] + shit[2] * shit[3] + shit[4] * shit[5]);
+	coef[2] = ft_sq(shit[1]) + ft_sq(shit[3]) + ft_sq(shit[5]) - ft_sq(cy.d / 2);
 	if (!(ft_solve_quadra(coef[0], coef[1], coef[2], root)))
 		return (res);
 	if (root[0] > 0.000001)
@@ -46,7 +52,7 @@ t_intersec	ft_cy_side(t_ray ray, t_cylinder cy)
 		u = ft_2p_to_v(cy.p, res.p);
 		if (ft_dot(cy.v, u) > 0 && ft_dot(cy.v, ft_2p_to_v(ft_add_td_n(cy.p, cy.v, cy.h), res.p)) < 0)
 		{
-			res.dist = ft_2p_dist(ray.p, res.p);
+			res.dist = root[0];
 			res.normal = ft_2p_to_v(ft_multi_td(cy.v, ft_dot(u, cy.v)), u);
 		}
 	}
@@ -56,7 +62,7 @@ t_intersec	ft_cy_side(t_ray ray, t_cylinder cy)
 		u = ft_2p_to_v(cy.p, storage.p);
 		if (ft_dot(cy.v, u) > 0 && ft_dot(cy.v, ft_2p_to_v(ft_add_td_n(cy.p, cy.v, cy.h), res.p)) < 0)
 		{
-			storage.dist = ft_2p_dist(ray.p, storage.p);
+			storage.dist = root[1];
 			res.normal = ft_2p_to_v(ft_multi_td(cy.v, ft_dot(u, cy.v)), u);
 		}
 	}
@@ -64,40 +70,3 @@ t_intersec	ft_cy_side(t_ray ray, t_cylinder cy)
 		res = storage;
 	return (res);
 }
-
-/*
-t_intersec	ft_cy_side(t_ray ray, t_cylinder cy)
-{
-	t_td	u;
-	t_td	v;
-	double	coef[3];
-	double	root[2];
-	t_intersec	res;
-	t_intersec	storage;
-	double	d1;
-	double	d2;
-
-	res.dist = -1;
-	v = ft_multi_td(cy.v, ft_dot(ray.v, cy.v));
-	v = ft_add_td_n(ray.v, v, -1);
-	u = ft_multi_td(cy.v, ft_dot(ft_add_td_n(ray.p, cy.p, -1), cy.v));
-	u = ft_add_td_n(ft_add_td_n(ray.p, cy.p, -1), u, -1);
-	coef[0] = ft_dot(v, v);
-	coef[1] = 2 * ft_dot(v, u);
-	coef[2] = ft_dot(u, u) - ft_sq(cy.d / 2);
-	if (!(ft_solve_quadra(coef[0], coef[1], coef[2], root)))
-		return res;
-	d1 = ft_dot(cy.v, ft_2p_to_v(cy.p, ft_add_td_n(ray.p, ray.v, root[0])));
-	d2 = ft_dot(cy.v, ft_add_td_n(ft_add_td_n(ray.p, ray.v, root[1]), cy.p, -1));
-	if (!((d1 > 0.000001 && d1 < cy.h + 0.000001 && root[0] > 0.000001) || (d2 > 0.000001 && d2 < cy.h + 0.000001 && root[1] > 0.000001)))
-		return (res);
-	res.p = ft_2p_to_v(cy.p, ft_add_td_n(ray.p, ray.v, root[0]));
-	res.dist = ft_2p_dist(ray.p, res.p);
-	res.normal = ft_unit_v(ft_add_td_n(ft_add_td_n(ft_multi_td(ray.v, root[0]), ft_multi_td(cy.v, d1), -1), ft_add_td_n(cy.p, ray.p, -1), -1));
-	storage.p = ft_2p_to_v(cy.p, ft_add_td_n(ray.p, ray.v, root[1]));
-	storage.dist = ft_2p_dist(ray.p, storage.p);
-	storage.normal = ft_unit_v(ft_add_td_n(ft_add_td_n(ft_multi_td(ray.v, root[1]), ft_multi_td(cy.v, d2), -1), ft_add_td_n(cy.p, ray.p, -1), -1));
-	if (res.dist == -1 || (storage.dist < res.dist && storage.dist != -1))
-		res = storage;
-	return (res);
-}*/
